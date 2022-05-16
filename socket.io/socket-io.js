b53bedeeb37d55821edd955a7af3678bf923cc0f
zhiming zhou <zzhou90@sheffield.ac.uk>
2022-05-16 14:19:02 +0100

    I make some change

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -3,6 +3,11 @@ exports.init = function(io) {
   io.sockets.on('connection', function (socket) {
     try {
      // insert here your event
+      socket.on('create or join', function (room, userId) {
+        socket.join(room);
+        chat.to(room).emit('joined', room, userId);
+      });
+
     } catch (e) {
     }
   });
